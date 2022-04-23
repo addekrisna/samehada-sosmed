afa32794f31b2c7974f87240c831da2efb470c1a
Adde Krisnamurtti <addekrisna@gmail.com>
2022-04-23 11:41:08 +0800

    adding hostname/server identity

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+echo gethostname(); // may output e.g,: sandie
+
+// Or, an option that also works before PHP 5.3
+echo php_uname('n'); // may output e.g,: sandie
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
